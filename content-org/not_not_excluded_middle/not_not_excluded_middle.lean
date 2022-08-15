a607c168e03f10db20bc2c8b3c903be2dba1feaf
jose <jabelcastellanosjoo@unm.edu>
2022-08-15 13:37:27 -0600

    Update not_not_excluded_middle content.

--- content-org/not_not_excluded_middle/not_not_excluded_middle.lean
+++ content-org/not_not_excluded_middle/not_not_excluded_middle.lean
@@ -1,5 +1,5 @@
-theorem neg_neg_excludded_middle : 
-((p ∨ (p → false)) → false) → false 
+theorem not_not_excludded_middle 
+: ((p ∨ (p → false)) → false) → false 
 :=
 let subproof := 
 fun hyp : (p ∨ (p → false)) → false =>
@@ -7,3 +7,13 @@ fun hp : p =>
 hyp (Or.intro_left (p -> false) hp)
 fun hyp : (p ∨ (p → false)) → false =>
 hyp (Or.intro_right p (subproof hyp))
+
+def composition (f : Y → Z) (g : X → Y) : X → Z :=
+fun x : X => f (g x)
+
+theorem not_not_modus_ponens 
+(nnp : (p → false) → false) (nnimp : ((p → q) → false) → false) : (q → false) → false :=
+let subproof (arg : q → false) (imp : p → q) : p → false :=
+fun hp : p => arg (imp hp)
+fun arg : q → false =>
+nnimp (composition nnp (subproof arg))
